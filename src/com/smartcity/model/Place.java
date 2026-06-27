c687bc0f0453340024dec8f91a33120d07d85448
JhansiOruganti-43 <jhansioruganti43@gmail.com>
2026-06-27 22:22:05 +0530

    refactor: make model fields private and add getters/setters

--- src/com/smartcity/model/Place.java
+++ src/com/smartcity/model/Place.java
@@ -1,18 +1,57 @@
 package com.smartcity.model;
 
 public class Place {
-    public int id;
-    public String name;
-    public String category;
-    public String location;
-    public String description;
+    private int id;
+    private String name;
+    private String category;
+    private String location;
+    private String description;
 
-    // Constructor to create a new place
     public Place(int id, String name, String category, String location, String description) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.location = location;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
--- src/com/smartcity/model/User.java
+++ src/com/smartcity/model/User.java
@@ -1,14 +1,36 @@
 package com.smartcity.model;
 
 public class User {
-    public String username;
-    public String password;
-    public String role;
 
-    // Constructor to create a new user
+    private String username;
+    private String password;
+    private String role;
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.role = "USER"; // Default role
+        this.role = "USER";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // No public setPassword() as per issue requirements
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
